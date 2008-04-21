d77616b0abf6db11eab978cab696e19c13ddef21
apel <apel>
2008-04-21 22:36:24 +0000

    *** empty log message ***

--- fstcomp/examples/Tests/Base/FooBar.cs
+++ fstcomp/examples/Tests/Base/FooBar.cs
@@ -1,3 +1,11 @@
-class FooBar {
-		
+using System; //NameSpace
+using System.Text;
+using Project = PC.MyCompany.Project;
+
+class HelloCsharp
+{
+    public static void Main()
+    {
+     Console.WriteLine("Hello CSharp World");
+    }
 }
