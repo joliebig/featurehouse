07fc47a373b3908b812b4d78b7d70970228fed16
boxleitn <boxleitn>
2008-07-22 21:02:56 +0000

    init test file

---
+++ fstcomp/modification/test/FSTParserTest.java
@@ -0,0 +1,23 @@
+package modification;
+
+public class FSTParserTest {
+
+    private int a, b, c;
+
+    private String k, l, m;
+
+    private void foo() {
+
+    }
+
+    private void bar() {
+
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public void print(){
+	System.out.println("BÄM");
+    }
+}
