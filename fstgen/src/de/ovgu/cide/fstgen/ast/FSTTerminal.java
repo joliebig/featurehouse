521fbfd0e9a014301f30c49dcb32bd1dff0a163c
boxleitn <boxleitn>
2008-09-17 11:58:41 +0000

    BerkeleyDB case study bugfixing äktschn...

--- fstgen/src/de/ovgu/cide/fstgen/ast/FSTTerminal.java
+++ fstgen/src/de/ovgu/cide/fstgen/ast/FSTTerminal.java
@@ -1,78 +1,83 @@
 package de.ovgu.cide.fstgen.ast;
 
-
 public class FSTTerminal extends FSTNode {
-	
-	public final static String defaultCompositionMechanism = "Replacement"; 
-	
-	private String body;
-	private String compose = defaultCompositionMechanism;
-	private String prefix;
-	
-	public FSTTerminal(String type, String name, String body, String prefix) {
-		super(type, name);
-		this.body = body;
-		this.prefix = prefix;
-	}
 
-	public FSTTerminal(String type, String name, String body, String prefix,
-			String compositionMechanism) {
-		this(type, name, body, prefix);
-		this.compose = compositionMechanism;
-	}
-	
-	public String getSpecialTokenPrefix() {
-		return prefix;
-	}
-	
-	@Override
-	public FSTNode getShallowClone() {
-		return new FSTTerminal(getType(), getName(), getBody(), getSpecialTokenPrefix(), getCompositionMechanism());
-	}
+    public final static String defaultCompositionMechanism = "Replacement";
 
-	@Override
-	public FSTNode getDeepClone() {
-		return new FSTTerminal(getType(), getName(), getBody(), getSpecialTokenPrefix(), getCompositionMechanism());
-	}
+    private String body;
 
-	public void setBody(String b) {
-		body = b;
-	}
+    private String compose = defaultCompositionMechanism;
 
-	public String getBody() {
-		return body;
-	}
+    private String prefix;
 
-	public String getCompositionMechanism() {
-		return compose;
-	}
+    public FSTTerminal(String type, String name, String body, String prefix) {
+	super(type, name);
+	this.body = body;
+	this.prefix = prefix;
+    }
 
-	@Override
-	public String toString() {
-		return "[T -> "
-				+ getName()
-				+ " : "
-				+ getType()
-				+ " \""
-				//+ (prefix.length() != 0 ? prefix.replaceAll("\\s", " ")	: "")
-				//+ "\" \""
-				+ (body.length() != 0 ? body.replaceAll("\\s", " ") : "")
-				+ "\" compose:" + compose + "]";
-	}
+    public FSTTerminal(String type, String name, String body, String prefix,
+	    String compositionMechanism) {
+	this(type, name, body, prefix);
+	this.compose = compositionMechanism;
+    }
 
-	public String printFST(int indent) {
-		StringBuffer buffer = new StringBuffer();
-		for (int i = 0; i < indent; i++)
-			buffer.append("    ");
-		buffer.append(this.toString());
-		buffer.append("\n");
-		return buffer.toString();
-	}
-	
-	@Override
-	public void accept(FSTVisitor visitor) {
-		visitor.visit(this);
-		visitor.postVisit(this);
-	}
+    public String getSpecialTokenPrefix() {
+	return prefix;
+    }
+
+    @Override
+    public FSTNode getShallowClone() {
+	return new FSTTerminal(getType(), getName(), getBody(),
+		getSpecialTokenPrefix(), getCompositionMechanism());
+    }
+
+    @Override
+    public FSTNode getDeepClone() {
+	return new FSTTerminal(getType(), getName(), getBody(),
+		getSpecialTokenPrefix(), getCompositionMechanism());
+    }
+
+    public void setBody(String b) {
+	body = b;
+    }
+
+    public String getBody() {
+	return body;
+    }
+
+    public String getCompositionMechanism() {
+	return compose;
+    }
+
+    @Override
+    public String toString() {
+	return "[T -> " + getName() + " : " + getType()
+		+ " \""
+		// + (prefix.length() != 0 ? prefix.replaceAll("\\s", " ") : "")
+		// + "\" \""
+		+ (body.length() != 0 ? body.replaceAll("\\s", " ") : "")
+		+ "\" compose:" + compose + "]";
+    }
+
+    public String printFST(int indent) {
+	StringBuffer buffer = new StringBuffer();
+	for (int i = 0; i < indent; i++)
+	    buffer.append("    ");
+	buffer.append(this.toString());
+	buffer.append("\n");
+	return buffer.toString();
+    }
+
+    @Override
+    public void accept(FSTVisitor visitor) {
+	visitor.visit(this);
+	visitor.postVisit(this);
+    }
+
+    public void setCompositionMechanism(String compositionMechanism) {
+	compose = compositionMechanism;
+
+    }
 
 }
