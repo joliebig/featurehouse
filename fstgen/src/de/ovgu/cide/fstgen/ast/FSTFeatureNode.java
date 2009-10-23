22e68966d242970d536bb65e5be78aa14267c6cb
apel <apel>
2009-10-23 17:06:02 +0000

    *** empty log message ***

--- fstgen/src/de/ovgu/cide/fstgen/ast/FSTFeatureNode.java
+++ fstgen/src/de/ovgu/cide/fstgen/ast/FSTFeatureNode.java
@@ -1,5 +1,7 @@
 package de.ovgu.cide.fstgen.ast;
 
+import java.util.LinkedList;
+
 public class FSTFeatureNode extends FSTNonTerminal {
 	public FSTFeatureNode(String name) {
 		super("Feature", name);
@@ -7,4 +9,8 @@ public class FSTFeatureNode extends FSTNonTerminal {
 	public boolean compatibleWith(FSTNode node) {
 		return this.getType().equals(node.getType());
 	}
+	
+    public FSTNode getShallowClone() {
+    	return new FSTFeatureNode(getName());
+    }
 }
