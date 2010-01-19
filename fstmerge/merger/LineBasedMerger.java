599a9a0ce1c60662648ce8ff77af809c581e9267
apel <apel>
2010-01-19 09:46:57 +0000

    *** empty log message ***

--- fstmerge/merger/LineBasedMerger.java
+++ fstmerge/merger/LineBasedMerger.java
@@ -90,8 +90,9 @@ public class LineBasedMerger implements MergerInterface {
 	        //	removeComments(fileVar2);
 	        //}
 	        
-	        String mergeCmd = "C:\\Programme\\cygwin\\bin\\merge.exe -q -p " + "\"" + fileVar1.getPath() + "\"" + " " + "\"" + fileBase.getPath() + "\"" + " " + "\"" + fileVar2.getPath() + "\"";// + " > " + fileVar1.getName() + "_output";
-			Runtime run = Runtime.getRuntime();
+	        //String mergeCmd = "C:\\Programme\\cygwin\\bin\\merge.exe -q -p " + "\"" + fileVar1.getPath() + "\"" + " " + "\"" + fileBase.getPath() + "\"" + " " + "\"" + fileVar2.getPath() + "\"";// + " > " + fileVar1.getName() + "_output";
+	        String mergeCmd = "merge -q -p " + fileVar1.getPath() + " " + fileBase.getPath() + " " + fileVar2.getPath();// + " > " + fileVar1.getName() + "_output";
+	        Runtime run = Runtime.getRuntime();
 			Process pr = run.exec(mergeCmd);
 			//pr.waitFor();
 			
