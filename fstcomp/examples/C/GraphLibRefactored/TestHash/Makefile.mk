796a8ea9de648780e7d5bedd9b5425f0b83c4d3b
apel <apel>
2010-03-30 10:57:21 +0000

    *** empty log message ***

--- fstcomp/examples/C/GraphLibRefactored/TestHash/Makefile.mk
+++
@@ -1,7 +0,0 @@
-test_graph_hash: test_graph.c graph_hash.h graph_hash.o
-	gcc ${GCCOPTS} `pkg-config --cflags --cflags --libs glib-2.0` test_graph.c graph_hash.o -o test_graph
-	@echo
-	time ./test_graph 10000 1000000
-
-graph_hash.o: graph_hash.h graph_hash.c
-	gcc ${GCCOPTS} `pkg-config --cflags --cflags glib-2.0` -c graph_hash.c 
--- fstcomp/examples/C/GraphLibRefactored/TimingBitMatrix/graph_bitmatrix.h
+++
@@ -1,6 +0,0 @@
-//! initialize timing
-double graph_timing_init(FILE *f);
-
-//! finalize timing
-double graph_timing_end(FILE *f);
-
