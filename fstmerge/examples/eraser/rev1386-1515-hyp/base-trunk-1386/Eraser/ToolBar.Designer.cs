83a03441a9bc6a3b60f22f8101351ef849380acf
joliebig <joliebig>
2010-07-13 10:57:43 +0000

    check in csharp projects eraser and processhacker

---
+++ fstmerge/examples/eraser/rev1386-1515-hyp/base-trunk-1386/Eraser/ToolBar.Designer.cs
@@ -0,0 +1,40 @@
+
+
+namespace Eraser
+{
+ partial class ToolBar
+ {
+
+
+
+  private System.ComponentModel.IContainer components = null;
+
+
+
+
+
+  protected override void Dispose(bool disposing)
+  {
+   if (disposing && (components != null))
+   {
+    components.Dispose();
+   }
+   base.Dispose(disposing);
+  }
+
+
+
+
+
+
+
+  private void InitializeComponent()
+  {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+  }
+
+
+ }
+}
