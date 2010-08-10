cf4623146ea03aacd84ef01c37fb916d5f5d2152
apel <apel>
2010-08-10 17:47:56 +0000

    *** empty log message ***

--- fstcomp/examples/Java/countlocfeat.sh
+++ fstcomp/examples/Java/countlocfeat.sh
@@ -4,14 +4,14 @@ PROJECTS=`find -maxdepth 1 -type d`
 for PRJ in $PROJECTS; do
 	if [[ $PRJ != . ]]
 	then
-		echo "########################"
-		echo "# " `basename $PRJ`
-		echo "########################"
 		FEATURES=`find $PRJ -maxdepth 1 -type d`
 		for FEATURE in $FEATURES; do
 			if [[ $FEATURE != $PRJ ]]
 			then
-				echo `basename $FEATURE` `find $FEATURE -name "*.java" | xargs grep -v ^$ | wc -l`
+				if [[ `basename $FEATURE` != CVS ]]
+				then			
+					echo `basename $PRJ` `basename $FEATURE` `find $FEATURE -name "*.java" | xargs grep -v ^$ | wc -l`
+				fi
 			fi
 		done
 	fi
