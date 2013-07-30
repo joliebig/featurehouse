03fd345b57386bca448f9e0cd97904edddc583a2
meinicke <jensmeinicke@gmx.de>
2013-07-30 15:25:06 +0200

    removed field invariant

--- fstcomp/composer/rules/meta/FieldOverridingMeta.java
+++ fstcomp/composer/rules/meta/FieldOverridingMeta.java
@@ -32,7 +32,7 @@ public class FieldOverridingMeta extends FieldOverriding {
 			}
 			terminalComp.setBody(compBody);
 		} else {
-			// TODO implement this could happen for alternative features
+			// TODO implement this can happen for alternative features
 			System.err.println("Error: compound field declarations cannot be composed: \'" + bodyA + "\' and \'" + bodyB + "\'");
 		}
 
@@ -41,57 +41,54 @@ public class FieldOverridingMeta extends FieldOverriding {
 	private String compose(FSTTerminal terminalA, FSTTerminal terminalB, FSTTerminal terminalComp, String bodyA, String bodyB) {
 		String fieldName = bodyB.substring(0,bodyB.indexOf('=')).trim();
 		fieldName = fieldName.substring(fieldName.lastIndexOf(' '));
-		String fieldA = bodyA.substring(bodyA.indexOf('=') + 1).replace(";", "").trim();
+//		String fieldA = bodyA.substring(bodyA.indexOf('=') + 1).replace(";", "").trim();
 		
-		FSTTerminal invariant = findInvariant("#" + fieldName, terminalComp);
-		if (invariant != null) {
-			String oldInvariant = invariant.getBody().substring(invariant.getBody().indexOf("FM.FeatureModel.") - 2, invariant.getBody().length() - 1);
-			String newInavariant = "invariant (FM.FeatureModel." + getFeatureName(terminalA) + " ==> " + fieldName + " == " + fieldA + 
-					") && (!FM.FeatureModel." + getFeatureName(terminalA) + " ==> (" + oldInvariant + ");";
-			invariant.setBody(newInavariant);
-		} else if (bodyA.contains("final ")) {
-			String fieldB = bodyB.substring(bodyB.indexOf('=') + 1).replace(";", "").trim();
-			String prefix = " ==> (" + fieldName + " == " + fieldB + ")";
-			String newPrefix = "invariant (FM.FeatureModel." + getFeatureName(terminalA) + " ==> " + fieldName + " == " + fieldA + 
-					") && (!FM.FeatureModel." + getFeatureName(terminalA) + prefix + ");";
-			if (bodyA.contains("static ")) {
-				newPrefix = "static " + newPrefix;
-			}
-			FSTNonTerminal decl = new FSTNonTerminal("ClassOrInterfaceBodyDeclaration1", "auto7");
-			((FSTNonTerminal)((FSTNonTerminal) terminalComp.getParent()).getParent()).addChild(decl);
-			FSTNonTerminal jml = new FSTNonTerminal("JMLDeclaration1", "#" + fieldName);
-			decl.addChild(jml);
-			jml.addChild(new FSTTerminal("Invariant", "-", newPrefix, "", "InvariantComposition"));
-		}
-		
-		
-		
+//		FSTTerminal invariant = findInvariant("#" + fieldName, terminalComp);
+//		if (invariant != null) {
+//			String oldInvariant = invariant.getBody().substring(invariant.getBody().indexOf("FM.FeatureModel.") - 2, invariant.getBody().length() - 1);
+//			String newInavariant = "invariant (FM.FeatureModel." + getFeatureName(terminalA) + " ==> " + fieldName + " == " + fieldA + 
+//					") && (!FM.FeatureModel." + getFeatureName(terminalA) + " ==> (" + oldInvariant + ");";
+//			invariant.setBody(newInavariant);
+//		} else if (bodyA.contains("final ")) {
+//			String fieldB = bodyB.substring(bodyB.indexOf('=') + 1).replace(";", "").trim();
+//			String prefix = " ==> (" + fieldName + " == " + fieldB + ")";
+//			String newPrefix = "invariant (FM.FeatureModel." + getFeatureName(terminalA) + " ==> " + fieldName + " == " + fieldA + 
+//					") && (!FM.FeatureModel." + getFeatureName(terminalA) + prefix + ");";
+//			if (bodyA.contains("static ")) {
+//				newPrefix = "static " + newPrefix;
+//			}
+//			FSTNonTerminal decl = new FSTNonTerminal("ClassOrInterfaceBodyDeclaration1", "auto7");
+//			((FSTNonTerminal)((FSTNonTerminal) terminalComp.getParent()).getParent()).addChild(decl);
+//			FSTNonTerminal jml = new FSTNonTerminal("JMLDeclaration1", "#" + fieldName);
+//			decl.addChild(jml);
+//			jml.addChild(new FSTTerminal("Invariant", "-", newPrefix, "", "InvariantComposition"));
+//		}
 		return bodyA.substring(0, bodyB.indexOf('=') + 1) + " FM.FeatureModel." + getFeatureName(terminalA) +
 				   " ? " + bodyA.substring(bodyA.indexOf('=') + 1).replaceAll(";", "") + " : " + bodyB.substring(bodyB.indexOf('=') + 1);
 	}
 	
-	/**
-	 * TODO description
-	 * @param string
-	 * @param terminalComp
-	 * @return
-	 */
-	private FSTTerminal findInvariant(String string, FSTTerminal terminalComp) {
-		FSTNonTerminal fieldDecl = ((FSTNonTerminal)terminalComp.getParent());
-		FSTNonTerminal classDeclaration = ((FSTNonTerminal)fieldDecl.getParent());
-		for (FSTNode child : classDeclaration.getChildren()) {
-			if (child instanceof FSTNonTerminal) {
-				FSTNonTerminal nonTerminal = (FSTNonTerminal) child;
-				if (nonTerminal.getType().equals("ClassOrInterfaceBodyDeclaration1")) {
-					FSTNonTerminal jMLDeclaration = (FSTNonTerminal)nonTerminal.getChildren().get(0);
-					if (jMLDeclaration.getName().equals(string)) {
-						return (FSTTerminal)(jMLDeclaration).getChildren().get(0);
-					}
-				}
-			}
-		}
-		return null;
-	}
+//	/**
+//	 * TODO description
+//	 * @param string
+//	 * @param terminalComp
+//	 * @return
+//	 */
+//	private FSTTerminal findInvariant(String string, FSTTerminal terminalComp) {
+//		FSTNonTerminal fieldDecl = ((FSTNonTerminal)terminalComp.getParent());
+//		FSTNonTerminal classDeclaration = ((FSTNonTerminal)fieldDecl.getParent());
+//		for (FSTNode child : classDeclaration.getChildren()) {
+//			if (child instanceof FSTNonTerminal) {
+//				FSTNonTerminal nonTerminal = (FSTNonTerminal) child;
+//				if (nonTerminal.getType().equals("ClassOrInterfaceBodyDeclaration1")) {
+//					FSTNonTerminal jMLDeclaration = (FSTNonTerminal)nonTerminal.getChildren().get(0);
+//					if (jMLDeclaration.getName().equals(string)) {
+//						return (FSTTerminal)(jMLDeclaration).getChildren().get(0);
+//					}
+//				}
+//			}
+//		}
+//		return null;
+//	}
 
 	private static String getFeatureName(FSTNode node) {
 		if (node.getType().equals("Feature"))
