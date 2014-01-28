8be90aaa529e4770cbeb96bae18b06d2b79b5abc
Jörg Liebig <joliebig@fim.uni-passau.de>
2014-01-28 15:46:54 +0100

    revert change e545f8936761b28cceed2074c61fb2039d8a339c on file, because it breaks the merging code

--- fstmerge/merger/LineBasedMerger.java
+++ fstmerge/merger/LineBasedMerger.java
@@ -58,12 +58,7 @@ public class LineBasedMerger implements MergerInterface {
 			
 			return;
 		}
-		
-		BufferedReader buf = null;
-		BufferedWriter writerVar1 = null;
-		BufferedWriter writerVar2 = null;
-		BufferedWriter writerBase = null;
-		Process pr = null;
+
 	    try {
 	    	long time = System.currentTimeMillis();
 	    	File tmpDir = new File(System.getProperty("user.dir") + File.separator + "fstmerge_tmp"+time);
@@ -73,23 +68,26 @@ public class LineBasedMerger implements MergerInterface {
 			File fileBase = File.createTempFile("fstmerge_base_", "", tmpDir);
 			File fileVar2 = File.createTempFile("fstmerge_var2_", "", tmpDir);
 			
-			writerVar1 = new BufferedWriter(new FileWriter(fileVar1));
+			BufferedWriter writerVar1 = new BufferedWriter(new FileWriter(fileVar1));
 	        if(node.getType().contains("-Content") || tokens[0].length() == 0)
 	        	writerVar1.write(tokens[0]);
 	        else 
 	        	writerVar1.write(tokens[0] + "\n");
+	        writerVar1.close();
 	        
-	        writerBase = new BufferedWriter(new FileWriter(fileBase));
+	        BufferedWriter writerBase = new BufferedWriter(new FileWriter(fileBase));
 	        if(node.getType().contains("-Content") || tokens[1].length() == 0)
 	        	writerBase.write(tokens[1]);
 	        else 
 	        	writerBase.write(tokens[1] + "\n");
+	        writerBase.close();
 
-	        writerVar2 = new BufferedWriter(new FileWriter(fileVar2));
+	        BufferedWriter writerVar2 = new BufferedWriter(new FileWriter(fileVar2));
 	        if(node.getType().contains("-Content") || tokens[2].length() == 0)
 	        	writerVar2.write(tokens[2]);
 	        else 
 	        	writerVar2.write(tokens[2] + "\n");
+	        writerVar2.close();
 
 	        String mergeCmd = ""; 
 	        if(System.getProperty("os.name").contains("Windows"))
@@ -97,21 +95,23 @@ public class LineBasedMerger implements MergerInterface {
 	        else
 	        	mergeCmd = "merge -q -p " + fileVar1.getPath() + " " + fileBase.getPath() + " " + fileVar2.getPath();// + " > " + fileVar1.getName() + "_output";
 	        Runtime run = Runtime.getRuntime();
-			pr = run.exec(mergeCmd);
-			
-			buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			Process pr = run.exec(mergeCmd);
+
+			BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 			String line = "";
 			String res = "";
 			while ((line=buf.readLine())!=null) {
 				res += line + "\n";
 			}
-
+			pr.getInputStream().close();
 			node.setBody(res);
 			
 			buf = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
 			while ((line=buf.readLine())!=null) {
 				System.err.println(line);
 			}
+			pr.getErrorStream().close();
+			pr.getOutputStream().close();
 	        
 		    fileVar1.delete();
 		    fileBase.delete();
@@ -119,45 +119,7 @@ public class LineBasedMerger implements MergerInterface {
 		    tmpDir.delete();
 
 	    } catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-	    	try {
-				closeStreams(buf, writerVar1, writerVar2, writerBase, pr);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	    	
+	    	e.printStackTrace();
 	    }
-	}
-
-	private void closeStreams(BufferedReader buf, BufferedWriter writerVar1,
-			BufferedWriter writerVar2, BufferedWriter writerBase, Process pr) throws IOException {
-		try {
-    		if (buf != null)buf.close();
-		} finally {
-			try {
-				if (writerVar1 != null)writerVar1.close();
-			} finally {
-				try {	
-					if (writerVar2 != null)writerVar2.close();
-				} finally {
-					try {
-						if (writerBase != null)writerBase.close();
-					} finally {
-						if (pr != null) {
-							try {
-								pr.getInputStream().close();
-							} finally {
-								try {
-									pr.getErrorStream().close();
-								} finally {
-									pr.getOutputStream().close();
-								}
-							}
-						}
-					}
-				}
-			}			
-    	}
 	}
 }
