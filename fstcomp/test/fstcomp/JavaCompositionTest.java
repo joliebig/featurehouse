4bd9ed0579aa8b672895b0e29431abbb2a40aab6
rhein <rhein@fim.uni-passau.de>
2014-08-13 15:26:47 +0200

    new test utils for testing in tmp directory; new test for bug with "abstract" keyword

--- fstcomp/test/fstcomp/JavaCompositionTest.java
+++ fstcomp/test/fstcomp/JavaCompositionTest.java
@@ -1,10 +1,17 @@
 package fstcomp;
 
-import static org.junit.Assert.*;
-import static fstcomp.ComposerTestUtil.*;
+import static fstcomp.ComposerTestUtil.compose;
+import static fstcomp.ComposerTestUtil.deleteDirWithContents;
+import static fstcomp.ComposerTestUtil.setupProductLineFeatures;
+import static org.junit.Assert.assertEquals;
+import fstcomp.ComposerTestUtil.Feature;
+import integrationtests.Checksum;
 
 import java.io.File;
-import integrationtests.Checksum;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class JavaCompositionTest {
@@ -18,6 +25,25 @@ public class JavaCompositionTest {
 		compose(expression, outputDir, null, null);
 		
 		assertEquals("9F4BC38FA03480D2C1144C1260A04E2A", Checksum.calculateChecksum(new File(outputDir)));
+	}
+	
+	@Test
+	public void testAbstractKeywordComposition() throws IOException {
+		// setup this minimal project in a tmp directory
+		List<Feature> features = new ArrayList<Feature>();
+		Feature a = new Feature("A"); features.add(a);
+		a.addFile("FileC.java", "public abstract class FileC {public abstract void m();}");
+		Feature b = new Feature("B"); features.add(b);
+		a.addFile("FileC.java", "public abstract class FileC {public abstract void m();}");
+		File mainDir = setupProductLineFeatures(features);
+		File outputDir = new File(mainDir, "output");
+		// compose with FH
+		compose(new File(mainDir, "features.exp").getAbsolutePath(), outputDir.getAbsolutePath(), new File(mainDir, "features").getAbsolutePath(), null);
+		// verify checksum of result
+		assertEquals("Checksum did not match. Generated files in " + mainDir.getAbsolutePath(),
+				"6992C64EB9E7F9F288FBEDB5C91381B6", Checksum.calculateChecksum(outputDir));
+		// if we arrive at this point, we can delete the tmp dir
+		deleteDirWithContents(mainDir);
 	}
 
 }
